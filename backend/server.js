1b3bb731f3e22a1a2ca6317aa050878ebaf2b9c6
adam.darcy <adam.darcy@workday.com>
2022-12-16 15:08:18 +0000

    added fix

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ async function prepare() {
       username VARCHAR NOT NULL,
       pw VARCHAR NOT NULL,
       email VARCHAR NOT NULL,
-      cdetails INT 
+      cdetails INT,
+      isadmin INT
     );
   `);
 
@@ -29,12 +30,12 @@ async function prepare() {
 }
 const prepared = prepare();
 
-async function set(username, pw, email, cdetails) {
+async function set(username, pw, email, cdetails, isadmin) {
     const id = `id-${username}`
     await prepared;
     await db.query(sql`
-    INSERT INTO users (id, username, pw, email, cdetails)
-      VALUES (${id}, ${username}, ${pw}, ${email}, ${cdetails})
+    INSERT INTO users (id, username, pw, email, cdetails, isadmin)
+      VALUES (${id}, ${username}, ${pw}, ${email}, ${cdetails}, ${isadmin})
     ON CONFLICT (id) DO UPDATE
       SET username=excluded.username;
   `);
@@ -63,7 +64,8 @@ async function run() {
     await set('Shane.Buffy'.toLowerCase(), 'password', 'shaneB\@lazynight.com', 11223344);
     await set('Caleb.Boe'.toLowerCase(), '1234', 'Caleb\@lazynight.com', 88776655);
     await set('Brian.Corrugated'.toLowerCase(), 'ironbru', 'brianC\@lazynight.com', 11998822);
-    await set('admin'.toLowerCase(), 'admin1234', 'admin\@lazynightadmin.com', 10203040);
+    await set('sean.ciara'.toLowerCase(), 'havoc', 'sean.c\@lazynight.com', 11445522);
+    await set('admin'.toLowerCase(), 'admin1234', 'admin\@lazynightadmin.com', 10203040, 1);
 }
 run().catch((ex) => {
     console.error(ex.stack);
@@ -83,13 +85,16 @@ app.use(cors({
     origin: '*'
 }));
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
 app.get('/login', (req, res) => {
 
     async function get() {
         await prepared;
         const user = req.query.user.toLowerCase();
         const pw = req.query.password.toLowerCase();
-        let query = 'SELECT username FROM users WHERE username="' + user + '" AND pw="' + pw + '";';
+        let query = 'SELECT username, isadmin FROM users WHERE username="' + user + '" AND pw="' + pw + '";';
         console.log(query)
         const results = await db.query(sql(query));
         console.log(results)
@@ -158,11 +163,11 @@ app.get('/sendFeedback', (req, res) => {
         const from = req.query.from.toLowerCase();
         const msg = req.query.msg.toLowerCase();
         const sender = req.query.sender.toLowerCase();
-        let query = 'INSERT INTO feedback values '+'("'+from+'","'+msg+'","'+sender+'");';
-        let ch = 'SELECT * from feedback'
+        let query = 'INSERT INTO feedback(user, msg, sender) values '+'("'+sender+'","'+msg+'","'+from+'");';
+        console.log(query)
+
         await db.query(sql(query));
-        const result = await db.query(sql(ch));
-        console.log(result)
+
         res.send(200)
     }
 
